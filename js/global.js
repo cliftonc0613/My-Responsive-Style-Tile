093971b23c7accf779d811f752de9452252e9ee6
Clifton Canady <hello@ctwebdesignshop.com>
2014-08-21 09:26:28 -0400

    added foundation js, ctwd logo and minified js and css

--- js/global.js
+++ js/global.js
@@ -1,9 +1,28 @@
 $("#owl-demo").owlCarousel({
 
-  autoPlay: 3000,
+  autoPlay: 5000,
 
   items : 4,
   itemsDesktop : [1199,3],
   itemsDesktopSmall : [979,3]
 
+});
+
+
+
+$(document).foundation();
+
+var doc = document.documentElement;
+doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+jQuery(window).scroll(function() {
+    if (scroll >= 50) {
+        $('#logo-image').attr('src', 'images/CTWDlogo.png');
+        $(".important-class").addClass("padding-on-my-header");
+    }
+    if (scroll < 50) {
+        $(".important-class").removeClass("padding-on-my-header");
+        $('#logo-image').attr('src', 'images/CTWDlogo.png');
+    }
 });
--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,8 +1,10 @@
 module.exports = {
   dist: {
     src: [
-      'js/libs/*.js',
-      'js/foundation.js',
+      'js/libs/jquery.js',
+      'js/libs/owl.carousel.js',
+      'js/libs/foundation.js',
+      'js/libs/foundation/*.js',
       'js/global.js'
     ],
     dest: 'js/build/production.js'
