5eb5d3aa67ebc659a00ed28f467ef126a36dc720
Clifton Canady <hello@ctwebdesignshop.com>
2014-08-23 20:59:39 -0400

    add flickrbomb from zurb

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -7,6 +7,7 @@ module.exports = {
       'js/libs/foundation/*.js',
       'js/libs/jquery.easing.1.3.js',
       'js/libs/jquery.animatedscroll.js',
+      // 'js/libs/jquery.flickrbomb.js',
       'js/global.js'
     ],
     dest: 'js/build/production.js'
