87fbc70b1d7b76cfc0ca160ea167cf747b35062d
Clifton Canady <hello@ctwebdesignshop.com>
2017-03-26 15:55:34 -0400

    gulpfile.js and package.json

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+
