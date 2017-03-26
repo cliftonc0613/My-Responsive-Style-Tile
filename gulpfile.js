3a0d585141b642dbf00312a00edf5d8b2f6813f8
Clifton Canady <hello@ctwebdesignshop.com>
2017-03-26 16:29:56 -0400

    adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,21 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
 
+// ===== Variable for output directory
+var outputDir   = '/build/';
 
+// ===== JSHint Task
+gulp.task('jshint', function() {
+    return gulp.src('assets/javascript/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(notify('Jshint - Successful'));
+});
